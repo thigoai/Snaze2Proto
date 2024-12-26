4215e82707d648ad5b9298a0d778fe2d54570c94
thigoai <tfreire352@gmail.com>
2024-12-26 11:21:08 -0300

    maze class

--- Snake2Proto/src/br/ufrn/imd/modelo/Maze.java
+++ Snake2Proto/src/br/ufrn/imd/modelo/Maze.java
@@ -8,7 +8,6 @@ public class Maze {
 	
 	public void generateMaze(int[][] cells) {
 		mazeLevel = new ArrayList<>();
-		
 		for(int i = 0; i < cells.length; i++) {
 			for(int j = 0; j < cells[i].length; j++) {
 				if(cells[i][j] == 1) {
